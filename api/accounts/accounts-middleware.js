e6cbc5cc5ac31f40866afa1d9481c32908dd5c02
Emre Can Kızılateş <kizilatesemrecan@gmail.com>
2023-06-19 22:53:32 +0300

    db-node 1

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,12 +1,58 @@
+const { response } = require("express");
+const accountsModel = require("./accounts-model");
+
 exports.checkAccountPayload = (req, res, next) => {
   // KODLAR BURAYA
   // Not: Validasyon için Yup(şu an yüklü değil!) kullanabilirsiniz veya kendiniz manuel yazabilirsiniz.
-}
+  try{
+    let {budget,name} = req.body;
+    if(budget === undefined ||name === undefined){
+      res.status(400).json({message:"name and budget are required"});
+    }else{
+      if(req.body.name) name = req.body.name.trim();
+      if(name.lenght > 3 || name.lenght < 100){
+        res.status(400).json({message:"name of account must be between 3 and 100"});
+      }else if(typeof budget !== "number"){
+        res.status(400).json({message:"budget of account must be a number"});
+      }else if(budget < 0 || budget > 1000000){
+        res.status(400).json({message:"budget of account is too large or too small"});
+      }else{
+        req.body.name=name;
+        next();
+      }
+    }
+  }catch(error){
+    res.status(400).json({message:error.message});
+  }
+};
 
-exports.checkAccountNameUnique = (req, res, next) => {
+exports.checkAccountNameUnique = async (req, res, next) => {
   // KODLAR BURAYA
-}
+  try{
+    let isExist=false;
+    let existAccount = await accountsModel.getByName(req.body.name);
+    isExist = existAccount != null;
 
-exports.checkAccountId = (req, res, next) => {
+    if(isExist){
+      res.status(400).json({message:"that name is taken"});
+    }else{
+      next();
+    }
+  }catch(error){
+    next(error);
+  }
+};
+
+exports.checkAccountId = async (req, res, next) => {
   // KODLAR BURAYA
-}
+  try{
+    const existAccount = await accountsModel.getById(req.params.id);
+    if(!existAccount){
+      res.status(404).json({message:"account not found"});
+    }else{
+      next();
+    }
+  }catch(error){
+    next(error);
+  }
+};
--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,37 @@
-const getAll = () => {
-  // KODLAR BURAYA
-}
+const db = require("../../data/db-config");
+
+const getAll = async (limit,sortBy,sortDir) => {
+  limit = limit || await db("accounts").length;
+  sortBy = sortBy || "id";
+  sortDir = sortDir || "asc";
+  return db("accounts").orderBy(sortBy,sortDir).limit(limit);
+};
 
 const getById = id => {
   // KODLAR BURAYA
-}
+  //default select için return array formatındadır. First yapınca object {} formatında dönüyor.
+  return db("accounts").where("id",id).first();
+};
 
-const create = account => {
+const create = async (account) => {
   // KODLAR BURAYA
-}
+  const inserted = await db("accounts").insert(account);
+  return getById(inserted[0]);
+};
 
-const updateById = (id, account) => {
+const updateById = async (id, account) => {
   // KODLAR BURAYA
-}
+  await db("accounts").where("id",id).update(account);
+  return getById(id);
+};
 
-const deleteById = id => {
+const deleteById = (id) => {
   // KODLAR BURAYA
+  return db("accounts").where("id",id).del();
+};
+
+const getByName = (name)=>{
+  return db("accounts").where("name",name).first();
 }
 
 module.exports = {
@@ -24,4 +40,5 @@ module.exports = {
   create,
   updateById,
   deleteById,
+  getByName,
 }
